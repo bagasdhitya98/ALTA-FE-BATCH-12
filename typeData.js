cb49b6ff8e040a3cbe951d19d32cfd565014aee4
BagasAlterra <bagas.dhitya@alterra.id>
2023-01-25 14:40:07 +0700

    add boolean, object, array

--- typeData.js
+++ typeData.js
@@ -37,21 +37,75 @@
 // console.log("lengthOfName version 2 : ", lengthOfName_2);
 
 //TIPE DATA NUMBER
-let number = 123;
-let float = 12.3;
+// let number = 123;
+// let float = 12.3;
 
-let numberA = 20;
-let numberB = 20;
+// let numberA = 20;
+// let numberB = 20;
 
-let sum = 5 + 5;
-let difference = 4 - 2;
-let product = 8 * 2;
-let quotient = 10 / 2;
-let modulo = 20 % 5;
+// let sum = 5 + 5;
+// let difference = 4 - 2;
+// let product = 8 * 2;
+// let quotient = 10 / 2;
+// let modulo = 20 % 5;
 
-console.log("result of number A and B : ", numberA + numberB);
-console.log("result of sum : ", sum);
-console.log("result of difference : ", difference);
-console.log("result of product : ", product);
-console.log("result of quotient : ", quotient);
-console.log("result of modulo : ", modulo);
+// console.log("result of number A and B : ", numberA + numberB);
+// console.log("result of sum : ", sum);
+// console.log("result of difference : ", difference);
+// console.log("result of product : ", product);
+// console.log("result of quotient : ", quotient);
+// console.log("result of modulo : ", modulo);
+
+//TIPE DATA BOOLEAN
+let condition_1 = true;
+let condition_2 = false;
+console.log("kondisi 1 : ", condition_1);
+console.log("kondisi 2 : ", condition_2);
+
+let compare = 4 > 9;
+console.log(compare);
+
+//TIPE DATA NULL / UNDEFINED
+let undifinedVar;
+let nullVar = null;
+console.log("hasil undifined : ", undifinedVar);
+console.log("hasil nullvar : ", nullVar);
+
+//TIPE DATA OBJECT
+let laptop = {
+  merk: "Asus",
+  color: ["white", "black", "red", "blue"],
+  spec: {
+    ram: 16,
+    processor: "Intel i9",
+  },
+};
+console.log("identitas dari laptop : ", laptop);
+
+//TIPE DATA ARRAY
+let arrVar = []; // array kosong
+
+let fruits = ["apel", "jeruk", "mangga", "salak", "anggur"]; // array string
+let ages = [21, 22, 23, 24, 25]; // array number/integer
+let laptops = [laptop, laptop, laptop]; // array object
+let identity = ["john", 21, "doe", 25]; // array combination
+
+console.log("result of identity : ", identity);
+console.log("result of fruits : ", fruits);
+console.log("result of ages : ", ages);
+console.log("result of laptops : ", laptops);
+
+//CEK TIPE DATA
+let varStr = "abcdefgh";
+let varInt = 123456;
+let varUndefined;
+let varNull = null;
+let varObject = {};
+let varArray = [2, 3];
+
+console.log("varStr : ", typeof varStr);
+console.log("varInt : ", typeof varInt);
+console.log("varUndefined : ", typeof varUndefined);
+console.log("varNull : ", typeof varNull);
+console.log("varObject : ", typeof varObject);
+console.log("varArray : ", typeof varArray);
