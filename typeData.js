82b008c2ba47324fcd7149ee41ef574343dcaa7b
BagasAlterra <bagas.dhitya@alterra.id>
2023-01-25 11:49:17 +0700

    add type data

---
+++ typeData.js
@@ -0,0 +1,57 @@
+//VAR
+// var strVar = "apel";
+// var strVar = 45678;
+// console.log(strVar);
+
+//LET
+// let strLet = "apel";
+// strLet = 12345;
+// console.log(strLet);
+
+//CONST
+// const strConst = "apel";
+// const intConst = 789123;
+// console.log(intConst);
+
+//TIPE - TIPE PENULISAN VARIABEL
+// accountNumber : camel case
+// AccountNumber : pascal case
+
+//TIPE DATA STRING
+// let fruit = "lemon";
+// let accountNumber = 08828771;
+
+// let firstName = "John";
+// let secondName = "Doe";
+// let fullName = firstName + secondName;
+// console.log("Fullname of users without spacing is : ", fullName); //menggabungkan dua string tanpa spasi
+// console.log(
+//   "Fullname of users with spacing is : " + firstName + " " + secondName
+// ); //menggabungkan dua string dengan spasi
+
+// let name = "John Doe";
+// let lengthOfName_1 = name.length;
+// console.log("lengthOfName version 1 : ", lengthOfName_1);
+
+// let lengthOfName_2 = "John Doe".length;
+// console.log("lengthOfName version 2 : ", lengthOfName_2);
+
+//TIPE DATA NUMBER
+let number = 123;
+let float = 12.3;
+
+let numberA = 20;
+let numberB = 20;
+
+let sum = 5 + 5;
+let difference = 4 - 2;
+let product = 8 * 2;
+let quotient = 10 / 2;
+let modulo = 20 % 5;
+
+console.log("result of number A and B : ", numberA + numberB);
+console.log("result of sum : ", sum);
+console.log("result of difference : ", difference);
+console.log("result of product : ", product);
+console.log("result of quotient : ", quotient);
+console.log("result of modulo : ", modulo);
