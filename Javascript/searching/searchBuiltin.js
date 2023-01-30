77abacc5deb66de3104881ab39636eee43e8f6ff
BagasAlterra <bagas.dhitya@alterra.id>
2023-01-30 15:47:55 +0700

    add sorting

--- Javascript/searching/searchBuiltin.js
+++ Javascript/searching/searchBuiltin.js
@@ -7,13 +7,13 @@ function builtinSearch(array, target) {
 
 const randomNumber = [11, 45, 80, 10, 22, 47, 65, 99, 34];
 
-// console.log(builtinSearch(randomNumber, 45));
-// console.log(builtinSearch(randomNumber, 80));
+console.log(builtinSearch(randomNumber, 45));
+console.log(builtinSearch(randomNumber, 80));
 
-const originalNumber = [20, 15, 10, 9, 5, 2];
-const copyOfNumber = [...originalNumber].sort((a, b) => {
-  return a - b;
-});
+// const originalNumber = [20, 15, 10, 9, 5, 2];
+// const copyOfNumber = [...originalNumber].sort((a, b) => {
+//   return a - b;
+// });
 
-console.log(originalNumber);
-console.log(copyOfNumber);
+// console.log(originalNumber);
+// console.log(copyOfNumber);
