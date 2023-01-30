b2b3c4d5cf0de97a78f84f0c7391373bc6f5afc9
BagasAlterra <bagas.dhitya@alterra.id>
2023-01-30 16:47:04 +0700

    add unit testing

---
+++ Javascript/unit-test/Basic-Programming-Part2/Prob2/Problem2.test.js
@@ -0,0 +1,21 @@
+const studentScore = require("./Problem2");
+
+test("Test case 1", () => {
+  expect(studentScore(80)).toEqual("A");
+});
+
+test("Test case 2", () => {
+  expect(studentScore(75)).toEqual("B+");
+});
+
+test("Test case 3", () => {
+  expect(studentScore(111)).toEqual("invalid");
+});
+
+test("Test case 4", () => {
+  expect(studentScore(60)).toEqual("B");
+});
+
+test("Test case 5", () => {
+  expect(studentScore(30)).toEqual("D");
+});
