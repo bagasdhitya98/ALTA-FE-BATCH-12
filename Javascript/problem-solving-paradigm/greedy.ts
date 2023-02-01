0418c3b24c823d84bff6ba7e672b9da942a1920f
BagasAlterra <bagas.dhitya@alterra.id>
2023-02-01 16:33:26 +0700

    add greedy

--- Javascript/problem-solving-paradigm/greedy.ts
+++ Javascript/problem-solving-paradigm/greedy.ts
@@ -29,8 +29,8 @@ function minimumCoins(coins: number[], amount: number) {
   for (let i = 0; i < len; i++) {
     while (amount >= coins[i]) {
       amount -= coins[i];
-      console.log(amount);
-      console.log(result);
+      console.log("amount : ", amount);
+      console.log("res : ", result);
       result++;
     }
   }
@@ -39,3 +39,15 @@ function minimumCoins(coins: number[], amount: number) {
 }
 
 console.log(minimumCoins([25, 10, 5, 1], 42));
+
+// x + y + z = A
+// x*y*z = B
+// x^2 + y^2 + z^2 = C
+
+// 6 + 6 + 14 = 1 <= A
+// 6 * 6 * 14 = B
+// 6^2 + 6^2 + 14^2 = C <= 10000
+
+// 1 + 2 + 3 = 1 <= A
+// 1 * 2 * 3 = B
+// 1^2 * 2^2 * 3^2 = C <= 1000
