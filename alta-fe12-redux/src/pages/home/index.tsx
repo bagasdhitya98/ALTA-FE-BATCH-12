b64bc52146512e93c09924484debe5988d2049f1
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-02-27 18:25:58 +0700

    create pages

---
+++ alta-fe12-redux/src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+import Navbar from "../../components/Navbar";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Navbar />
+    </Layout>
+  );
+};
+
+export default Home;
